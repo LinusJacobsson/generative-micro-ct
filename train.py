82a6c11323c745981a5bff09629b3f38aec71368
LinusJacobsson <jacobssonlinus00@gmail.com>
2023-11-20 13:31:06 +0100

    added cl arguments

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import functools
 from torch.optim import Adam
@@ -7,46 +8,47 @@ import tqdm
 from tqdm import notebook
 from functions import ScoreNet, marginal_prob_std, diffusion_coeff, loss_fn, TumorDataSet
 
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Tumor Image Training Script')
+parser.add_argument('-sigma', type=float, default=25.0, help='Sigma value for the score network')
+parser.add_argument('-pixel_size', type=int, default=128, help='Pixel size for image resizing')
+parser.add_argument('-epochs', type=int, default=50, help='Number of training epochs')
+parser.add_argument('-learning_rate', type=float, default=1e-4, help='Learning rate')
+parser.add_argument('-batch_size', type=int, default=32, help='Batch size')
+
+# Parse arguments
+args = parser.parse_args()
 
 data_dir = '../../cropped_medium_res/'
 transform = transforms.Compose([
-    transforms.Resize((128, 128)),
+    transforms.Resize((args.pixel_size, args.pixel_size)),
     transforms.ToTensor(),
 ])
 
 dataset = TumorDataSet(data_dir, transform)
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
-
-
-sigma =  25.0#@param {'type':'number'}
+sigma = args.sigma
 marginal_prob_std_fn = functools.partial(marginal_prob_std, sigma=sigma)
 diffusion_coeff_fn = functools.partial(diffusion_coeff, sigma=sigma)
 
 score_model = torch.nn.DataParallel(ScoreNet(marginal_prob_std=marginal_prob_std_fn))
-#score_model = score_model.to(device)
 
-n_epochs =   50#@param {'type':'integer'}
-## size of a mini-batch
-batch_size =  32 #@param {'type':'integer'}
-## learning rate
-lr=1e-4 #@param {'type':'number'}
+n_epochs = args.epochs
+batch_size = args.batch_size
+lr = args.learning_rate
 
 optimizer = Adam(score_model.parameters(), lr=lr)
 tqdm_epoch = tqdm.trange(n_epochs)
 for epoch in tqdm_epoch:
-  avg_loss = 0.
-  num_items = 0
-  for x in dataloader:
-    #x = x.to(device)    
-    loss = loss_fn(score_model, x, marginal_prob_std_fn)
-    optimizer.zero_grad()
-    loss.backward()    
-    optimizer.step()
-    avg_loss += loss.item() * x.shape[0]
-    num_items += x.shape[0]
-  # Print the averaged training loss so far.
-  tqdm_epoch.set_description('Average Loss: {:5f}'.format(avg_loss / num_items))
-  # Update the checkpoint after each epoch of training.
-  torch.save(score_model.state_dict(), 'ckpt.pth')
-
+    avg_loss = 0.
+    num_items = 0
+    for x in dataloader:
+        loss = loss_fn(score_model, x, marginal_prob_std_fn)
+        optimizer.zero_grad()
+        loss.backward()    
+        optimizer.step()
+        avg_loss += loss.item() * x.shape[0]
+        num_items += x.shape[0]
+    tqdm_epoch.set_description('Average Loss: {:5f}'.format(avg_loss / num_items))
+    torch.save(score_model.state_dict(), 'ckpt.pth')
