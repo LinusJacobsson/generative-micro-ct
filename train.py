a539bd4ce72599969a69bc5433b1c9d8e8196116
dana2959 <david@valuedriven.se>
2023-11-20 17:08:22 +0100

    Fixed loss plot and saving of weights

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import torchvision.transforms as transforms
 import tqdm
 from tqdm import notebook
 from functions import ScoreNet, marginal_prob_std, diffusion_coeff, loss_fn, TumorDataSet
+import matplotlib.pyplot as plt
 
 # Set up argument parser
 parser = argparse.ArgumentParser(description='Tumor Image Training Script')
@@ -19,7 +20,7 @@ parser.add_argument('-batch_size', type=int, default=32, help='Batch size')
 # Parse arguments
 args = parser.parse_args()
 
-data_dir = '../../cropped_medium_res/'
+data_dir = 'C:/Users/David/OneDrive/Desktop/Test'
 transform = transforms.Compose([
     transforms.Resize((args.pixel_size, args.pixel_size)),
     transforms.ToTensor(),
@@ -40,6 +41,8 @@ lr = args.learning_rate
 
 optimizer = Adam(score_model.parameters(), lr=lr)
 tqdm_epoch = tqdm.trange(n_epochs)
+losses = []
+
 for epoch in tqdm_epoch:
     avg_loss = 0.
     num_items = 0
@@ -50,5 +53,14 @@ for epoch in tqdm_epoch:
         optimizer.step()
         avg_loss += loss.item() * x.shape[0]
         num_items += x.shape[0]
+    losses.append(avg_loss / num_items)
     tqdm_epoch.set_description('Average Loss: {:5f}'.format(avg_loss / num_items))
-    torch.save(score_model.state_dict(), 'ckpt.pth')
+
+    if (epoch + 1) % 100 == 0 or (epoch + 1) == n_epochs:
+        torch.save(score_model.state_dict(), 'ckpt.pth')
+
+plt.plot(range(1, n_epochs + 1), losses, label='Average loss')
+plt.xlabel('Epoch')
+plt.ylabel('Average loss')
+plt.title('Training loss over time')
+plt.savefig('Loss_plot.png')
